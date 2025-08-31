d063e0ab32e7f38e7a434a2776811acc99bd3c4c
Max <maximilian.gelbrecht@posteo.de>
2025-08-31 20:37:26 +0200

    fix tests

--- test/model_test.jl
+++ test/model_test.jl
@@ -8,9 +8,9 @@
     prob = ODEProblem(model, u0, tspan, p)
     sol = solve(prob, Tsit5(), saveat=0.005)
     @test sol.retcode == :Success
-    @test sol[end] != sol[1]         # something happended 
-    @test all(isfinite.(sol[end]))   # no blow up for default config 
-    @test (sum(abs.(sol[end])) .> 1) # no fixed point for the default config 
+    @test sol.u[end] != sol.u[1]         # something happended 
+    @test all(isfinite.(sol.u[end]))   # no blow up for default config 
+    @test (sum(abs.(sol.u[end])) .> 1) # no fixed point for the default config 
 end
 
 @testset "OneLayerLorenz96" begin
@@ -21,7 +21,7 @@ end
     prob = ODEProblem(model, u0, tspan, p)
     sol = solve(prob, Tsit5(), saveat=0.005)
     @test sol.retcode == :Success
-    @test sol[end] != sol[1]         # something happended 
-    @test all(isfinite.(sol[end]))   # no blow up for default config 
-    @test (sum(abs.(sol[end])) .> 1) # no fixed point for the default config 
+    @test sol.u[end] != sol.u[1]         # something happended 
+    @test all(isfinite.(sol.u[end]))   # no blow up for default config 
+    @test (sum(abs.(sol.u[end])) .> 1) # no fixed point for the default config 
 end
